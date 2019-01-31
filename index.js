d49d8e99d809b9e88e642a832819a5af24dc5905
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-31 21:57:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ if (katzDeliLine === undefined || katzDeliLine.length == 0) {
   katzDeliLine.shift();
 }
 }
+
+function currentLine (katzDeliLine){
+  if (katzDeliLine === undefined || katzDeliLine.length == 0) {
+    return "The line is currently empty.";
+  } else {
+    for (var i = 0; i < katzDeliLine.length; i++) {  
+      return "The line is currently: "+[i+1] ". "katzDeliLine[i] ",";
+}
+}  
+}
