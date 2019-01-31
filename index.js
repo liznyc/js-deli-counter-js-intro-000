2a0dd66e6b357c2b205e12e1eaec7b94cfed380a
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-01-31 21:40:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function takeANumber (katzDeliLine, name) {
+katzDeliLine.push(name);
+return "Welcome, " +[name]+". You are number "+[katzDeliLine.length] +" in line."
+}
+
+function nowServing (katzDeliLine) {
+if (katzDeliLine === undefined || katzDeliLine.length == 0) {
+  return "There is nobody waiting to be served!";
+} else {
+  return "Currently serving "+[katzDeliLine[0]]+".";
+  katzDeliLine.shift();
+}
+}
